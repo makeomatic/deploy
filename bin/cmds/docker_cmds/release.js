ec82a2391c1549fb6a1370fb8be118d637514fb9
AVVS <v@makeomatic.ru>
2017-03-30 20:21:35 -0700

    fix: correctly invoke handlers

--- bin/cmds/docker_cmds/release.js
+++ bin/cmds/docker_cmds/release.js
@@ -5,7 +5,7 @@
 exports.command = 'release';
 exports.desc = 'performs build, tagging and push in one operation';
 exports.handler = (argv) => {
-  require('./build')(argv);
-  require('./tag')(argv);
-  require('./push')(argv);
+  require('./build').handler(argv);
+  require('./tag').handler(argv);
+  require('./push').handler(argv);
 };
